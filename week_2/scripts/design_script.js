e1c0d20d55618da4eb3d2061bb59cc549788b136
BryceBlignaut <bryce.blignaut12@gmail.com>
2023-01-14 15:06:36 -0700

    week_2_commit

---
+++ week_2/scripts/design_script.js
@@ -0,0 +1,9 @@
+// 3 design documents Color, Balance, Repitition
+
+// This sets the new date in the footer
+
+const today = new Date();
+document.querySelector("#year").textContent = today.getFullYear();
+
+document.querySelector("#modified").textContent = document.lastModified; 
+
