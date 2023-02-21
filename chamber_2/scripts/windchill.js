764219d321f99c15794136dc9e6e53f8f0177bdb
BryceBlignaut <bryce.blignaut12@gmail.com>
2023-02-21 10:23:49 -0700

    last_visited

--- chamber_2/scripts/windchill.js
+++ chamber_2/scripts/windchill.js
@@ -18,3 +18,18 @@ function setWindChill(windspeed, temp){
 }
 
 setWindChill(10, 5);
+
+const MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+let today = new Date();
+let lastVisitString = localStorage.getItem("lastVisit");
+let visitspan = document.querySelector('#days-since-visit');
+
+if (lastVisitString==null){        
+    visitspan.textContent = '0'
+}
+else{
+    lastVisitDate=new Date(lastVisitString);
+    daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY);
+    visitspan.textContent = daysSinceLastVisit;
+}
+localStorage.setItem("lastVisit", today.toLocaleDateString());
