e60fe2b3aeafa9fffec982398c6c011e873ba469
BryceBlignaut <bryce.blignaut12@gmail.com>
2023-03-18 13:11:36 -0600

    final commit

--- chamber_2/scripts/header-footer.js
+++ chamber_2/scripts/header-footer.js
@@ -1,25 +1,21 @@
-// This sets today's date in the header
+// Today's date in the header
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
 document.querySelector(".header-today p").textContent = fulldate;
 
-// This sets the year for the footer
+
 const today = new Date();
 document.querySelector("#currentyear").textContent = today.getFullYear();
 
-// This sets the last modified date on the home page
 document.querySelector("#lastmodified").textContent = document.lastModified;
 
-// Toggle the menu open or closed
 function toggleMenu(){
     document.querySelector("nav ul").classList.toggle("menu-active");
     document.querySelector("#hamburger-x").classList.toggle("menu-active");
     document.querySelector("#hamburger-equiv").classList.toggle("menu-active");
 }
 
-// Attach click listener to the hamburger menu
 document.querySelector("#hamburger-menu").addEventListener('click', toggleMenu);
 
-// Pop up Banner Every Tuesday and Monday
 var messagedate = new Date();
 if(messagedate.getDay() == 1 || messagedate.getDay() == 2){
     document.querySelector(".banner").classList.add('active')
--- chamber_2/scripts/windchill.js
+++ chamber_2/scripts/windchill.js
@@ -1,20 +1,53 @@
-function setWindChill(windspeed, temp){        
-    // Get the DOM objects that are dynamic
-    tempSpan = document.querySelector("#temperature");
-    windSpeedSpan = document.querySelector("#windspeed");
-    windChillSpan = document.querySelector("#windchill");
+// Wind chill calculations
+function setWindChill(windSpeed, temperature) {
 
-    // Set up the wind chill content
-    let windchill = 'N/A';
-    if (windspeed >= 3.0 && temp <= 50){
-        let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-        windchill = `${chill}`;
+    let temperatureSpan = document.querySelector("#temperature");
+    let windSpeedSpan = document.querySelector("#windspeed");
+    let windChillSpan = document.querySelector("#windchill");
+  
+    let windChillMessage = "N/A";
+    if (windSpeed >= 3.0 && temperature <= 50) {
+      let chill = Math.round(35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16));
+      windChillMessage = `${chill}`;
     }
 
-    // Write out the dynamic content
-    tempSpan.textContent = temp;
-    windSpeedSpan.textContent = windspeed;
-    windChillSpan.innerHTML = windchill;
-}
-
-setWindChill(10, 5);
+    temperatureSpan.textContent = temperature;
+    windSpeedSpan.textContent = windSpeed;
+    windChillSpan.innerHTML = windChillMessage;
+  }
+  
+  const weatherIcon = document.querySelector("#weathericon");
+  const weatherDesc = document.querySelector("#weatherdesc");
+  
+  const LAT = "43.4799";
+  const LON = "-110.7624";
+  const APIKEY = "7be467cddb4e374f87db7a311fc5903e";
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}&units=imperial`;
+  
+  function displayResults(weatherData) {
+    // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`
+    const desc = weatherData.weather[0].description;
+    const main = weatherData.weather[0].main;
+    
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    weatherDesc.textContent = main; 
+    setWindChill(weatherData.wind.speed.toFixed(0), weatherData.main.temp.toFixed(0));
+  }
+  
+  async function getTheWeather() {
+    try {
+      const response = await fetch(apiURL);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  getTheWeather();
